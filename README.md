90d2e49f2fc7f5fc9ebd4b4fbceef15c2f1581fa
Michael Davies <mdavies35@gmail.com>
2023-09-20 21:44:30 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JackAndJillDoor
 A Raspberry pi project to handle a basic access control system for a 2 door bathroom
+
+TODO readme
---
+++ index.js
@@ -0,0 +1,81 @@
+var Gpio = require('onoff').Gpio; //require onoff to control GPIO
+const express = require('express'); //require express as adding a REST override for doors
+
+// phsical
+var pushButton1 = new Gpio(17, 'in', 'both', { debounceTimeout: 20 }); //use GPIO pin 17 as input
+var pushButton2 = new Gpio(18, 'in', 'both', { debounceTimeout: 20 }); //use GPIO pin 18 as input
+var LEDPin = new Gpio(4, 'out'); //declare GPIO4 an output
+var DOORPin1 = new Gpio(20, 'out'); //declare GPIO4 an output
+var DOORPin2 = new Gpio(21, 'out'); //declare GPIO4 an output
+var overrideButton = false;
+
+
+// api
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
+
+function getDoorStatus(doorPin) {
+    stat = DOORPin.readSync();
+    if (stat === 0) {
+        statusString = "Locked";
+    } else {
+        statusString = "Unlocked";
+    }
+    const status = {
+        "Status": statusString
+    };
+    return status;
+}
+const statusRoute = function (request, response, next) {
+    doorsStatus = {
+        door1: getDoorStatus(DOORPin1),
+        door2: getDoorStatus(DOORPin2),
+    }
+    response.send(doorsStatus);
+    next()
+};
+
+const unlockRoute = function (request, response, next) {
+    overrideButton = true;
+    DOORPin1.writeSync(1);
+    DOORPin2.writeSync(1);
+    next();
+};
+
+app.get("/status", [statusRoute]);
+
+app.get("/unlock", [unlockRoute, statusRoute]);
+
+var state = 0;
+pushButton.watch(function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
+    console.log("in:" + value + "," + state);
+    if (overrideButton) {
+        if (value === 1) {
+            overrideButton = false;
+        }
+    } else {
+        if (value === 0) {
+            state = 1;
+        } else {
+            state = 0;
+        }
+        // state = value == 1 ? 0 :1;
+        // state = value;
+        LEDPin.writeSync(state);
+        DOORPin1.writeSync(state);
+    }
+    console.log("out:" + value + "," + state);
+    console.log("end");
+});
+
+function unexportOnClose() { //function to run when exiting program
+    LEDPin.writeSync(0); // Turn LED off
+    LEDPin.unexport(); // Unexport LED GPIO to free resources
+    DOORPin1.writeSync(1);
+    DOORPin1.unexport();
+    pushButton.unexport(); // Unexport Button GPIO to free resources
+};
