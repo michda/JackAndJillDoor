11c2a47e843d6d93c0fb9070b78aa3d021d17026
Mcihael D <mdavies35@gmail.com>
2025-09-23 21:45:57 +0100

    refactor: update GPIO pin assignments and improve button handling logic

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ var Gpio = require('onoff').Gpio; //require onoff to control GPIO
 const express = require('express'); //require express as adding a REST override for doors
 
 // phsical
-var pushButton1 = new Gpio(17, 'in', 'both', { debounceTimeout: 20 }); //use GPIO pin 17 as input
-var pushButton2 = new Gpio(18, 'in', 'both', { debounceTimeout: 20 }); //use GPIO pin 18 as input
-var LEDPin = new Gpio(4, 'out'); //declare GPIO4 an output
-var DOORPin1 = new Gpio(20, 'out'); //declare GPIO20 an output
-var DOORPin2 = new Gpio(21, 'out'); //declare GPIO21 an output
+var pushButton1 = new Gpio(526, 'in', 'falling'); //use GPIO pin 14 as input
+var pushButton2 = new Gpio(527, 'in', 'falling'); //use GPIO pin 15 as input
+var DOORPin1 = new Gpio(531, 'out'); //declare GPIO19 an output
+var DOORPin2 = new Gpio(525, 'out'); //declare GPIO26 an output
 var overrideButton = false;
 
+// initialize to open
+DOORPin1.writeSync(1);
+DOORPin2.writeSync(1);
 
 // api
 const app = express();
@@ -39,10 +41,17 @@ const statusRoute = function (request, response, next) {
     next()
 };
 
-const unlockRoute = function (request, response, next) {
+const lockRoute = function (request, response, next) {
     overrideButton = true;
     DOORPin1.writeSync(1);
     DOORPin2.writeSync(1);
+    next();
+};
+
+const unlockRoute = function (request, response, next) {
+    overrideButton = true;
+    DOORPin1.writeSync(0);
+    DOORPin2.writeSync(0);
     next();
 };
 
@@ -53,41 +62,45 @@ const isolateRoute = function (request, response, next) {
 }
 
 app.get("/status", [statusRoute]);
-
+app.get("/lock", [lockRoute, statusRoute]);
 app.get("/unlock", [unlockRoute, statusRoute]);
 
 app.get("/isolate/:door", [isolateRoute, statusRoute]);
 
 var state = 0;
 
-const buttonWatch = function (err, valule, button) {
+const buttonWatch = function (err, value, button) {
     console.log("in:" + value + "," + state);
     if (overrideButton) {
         if (value === 1) {
             overrideButton = false;
         }
     } else {
-        if (value === 0) {
+        if (state === 0) {
             state = 1;
         } else {
             state = 0;
         }
-        // state = value == 1 ? 0 :1;
+        // state = value == 1 ? 0 : 1;
         // state = value;
-        LEDPin.writeSync(state);
+        // LEDPin.writeSync(state);
         DOORPin1.writeSync(state);
         DOORPin2.writeSync(state);
     }
     console.log("out:" + value + "," + state);
     console.log("end");
 }
-pushButton1.watch(function (err, value) { buttonWatch(err, value, pushButton1); });
-pushButton2.watch(function (err, value) { buttonWatch(err, value, pushButton2); });
+pushButton1.watch(function (err, value) { buttonWatch(err, state, pushButton1); });
+pushButton2.watch(function (err, value) { buttonWatch(err, state, pushButton2); });
 
 function unexportOnClose() { //function to run when exiting program
-    LEDPin.writeSync(0); // Turn LED off
-    LEDPin.unexport(); // Unexport LED GPIO to free resources
+    // LEDPin.writeSync(0); // Turn LED off
+    // LEDPin.unexport(); // Unexport LED GPIO to free resources
     DOORPin1.writeSync(1);
+    DOORPin2.writeSync(1);
     DOORPin1.unexport();
-    pushButton.unexport(); // Unexport Button GPIO to free resources
+    DOORPin2.unexport();
+    pushButton1.unexport(); // Unexport Button GPIO to free resources
+    pushButton2.unexport(); // Unexport Button GPIO to free resources
 };
+
